2d7178ac723eaa0a92eab61abeb3c8365b2fdf2c
Amarnath Baranwal <79193713+amarbaranwal123@users.noreply.github.com>
2021-10-04 16:49:40 +0530

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.topper = new System.Windows.Forms.Button();
+            this.submit = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtPhysics = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMath = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtResult);
+            this.groupBox1.Controls.Add(this.topper);
+            this.groupBox1.Controls.Add(this.submit);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.txtAge);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(271, 344);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Student_Data";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(97, 304);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(82, 20);
+            this.txtResult.TabIndex = 7;
+            // 
+            // topper
+            // 
+            this.topper.Location = new System.Drawing.Point(66, 269);
+            this.topper.Name = "topper";
+            this.topper.Size = new System.Drawing.Size(133, 29);
+            this.topper.TabIndex = 6;
+            this.topper.Text = "Show Topper";
+            this.topper.UseVisualStyleBackColor = true;
+            this.topper.Click += new System.EventHandler(this.topper_Click);
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(66, 214);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(133, 29);
+            this.submit.TabIndex = 5;
+            this.submit.Text = "Submit Details";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtPhysics);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtMath);
+            this.groupBox2.Location = new System.Drawing.Point(28, 116);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(213, 79);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Marks ";
+            // 
+            // txtPhysics
+            // 
+            this.txtPhysics.Location = new System.Drawing.Point(110, 50);
+            this.txtPhysics.Name = "txtPhysics";
+            this.txtPhysics.Size = new System.Drawing.Size(62, 20);
+            this.txtPhysics.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Physics :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Maths :";
+            // 
+            // txtMath
+            // 
+            this.txtMath.Location = new System.Drawing.Point(110, 21);
+            this.txtMath.Name = "txtMath";
+            this.txtMath.Size = new System.Drawing.Size(62, 20);
+            this.txtMath.TabIndex = 5;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(97, 68);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(145, 20);
+            this.txtAge.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Age :";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(97, 29);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(145, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 372);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button topper;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtPhysics;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMath;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Collections;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string temp = "";
+        int i = 0;
+        int x = 0;
+        int y = 0;
+        List<List<string>> arr = new List<List<string>>();
+        ArrayList result = new ArrayList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            int m = 0;
+            arr.Add(new List<string>()); //adds a new list to your list of lists
+
+            if (arr.Count == 1)
+            {
+                arr[i].Add(txtName.Text); //adds a string to the first list
+                arr[i].Add(txtAge.Text); //adds a string to the first list
+                arr[i].Add(txtMath.Text); //adds a string to the first list
+                arr[i].Add(txtPhysics.Text); //adds a string to the first list   
+                i++;
+            }
+            else
+            {
+                while (m < arr.Count-1)
+                {
+                    if (arr[m][0]==txtName.Text   && arr[m][1] == txtAge.Text   && arr[m][2]== txtMath.Text &&  arr[m][3]== txtPhysics.Text)
+                    {
+                        MessageBox.Show("Data Already Available");
+                       // --i;
+                        break;
+                    }
+                   
+                    m++;
+                }
+                arr[i].Add(txtName.Text); //adds a string to the first list
+                arr[i].Add(txtAge.Text); //adds a string to the first list
+                arr[i].Add(txtMath.Text); //adds a string to the first list
+                arr[i].Add(txtPhysics.Text); //adds a string to the first list
+                i++;
+            }
+            
+
+            txtName.Text = "";
+            txtAge.Text = "";
+            txtMath.Text = "";
+            txtPhysics.Text = "";
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void topper_Click(object sender, EventArgs e)
+        {
+            int m=0;
+            int index = 0;
+            dynamic num1;
+            dynamic num2;
+
+            while (m<arr.Count)
+            {
+                num1 = arr[m][2];
+                num2 = arr[m][3];
+                dynamic num = Convert.ToInt32(num1) + Convert.ToInt32(num2);
+                result.Insert(index++,num);
+                m++;
+            }
+ 
+            for (m = 1; m < result.Count; m++)
+            {
+                x =   Convert.ToInt32(result[m-1]);
+                y =  Convert.ToInt32(result[m]); 
+                if (x > y)
+                {
+                    temp = arr[m-1][0];
+                }
+            }
+            txtResult.Text = temp.ToString();
+        }       
+    }
+}
+
+/*
+ namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string temp = "";
+        int i = 0;
+        string[][] jArray = new string[5][];
+        int[] result = new int[5];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            jArray[i++] = new string[4] { txtName.Text, txtAge.Text, txtMath.Text, txtPhysics.Text };
+            MessageBox.Show((jArray.Length).ToString());
+            // MessageBox.Show("Data Inserted on index : "+ i.ToString());
+
+
+            txtName.Text = "";
+            txtAge.Text = "";
+            txtMath.Text = "";
+            txtPhysics.Text = "";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void topper_Click(object sender, EventArgs e)
+        {
+            int m = 0;
+            dynamic num1;
+            dynamic num2;
+
+            while (m<jArray.Length)
+            {
+                num1 =jArray[m][2];
+                num2 =jArray[m][3];
+                result[m] = Convert.ToInt32(num1) + Convert.ToInt32(num2);
+                m++;
+            }
+            temp = jArray[0][0];
+            for (m = 1; m < result.Length; m++)
+            {
+                if (result[m - 1] < result[m])
+                {
+                    temp = jArray[m][0];
+                }
+            }
+            txtResult.Text = temp.ToString();
+        }       
+    }
+}
+
+ */
+
+/*
+  result[0] = Convert.ToInt32(jArray[0][2]) + Convert.ToInt32(jArray[0][3]);
+            result[1] = Convert.ToInt32(jArray[1][2]) + Convert.ToInt32(jArray[1][3]);
+            result[2] = Convert.ToInt32(jArray[2][2]) + Convert.ToInt32(jArray[2][3]);
+            temp = jArray[0][0];
+            for (int m = 1; m < 3; m++)
+            {
+                if (result[m - 1] < result[m])
+                {
+                    temp = jArray[m][0];
+                }
+            }
+            txtResult.Text = temp.ToString();
+            //MessageBox.Show("Topper is : " + temp.ToString());
+ */
+
+/*  string temp;
+              int a = 0, b = 0;
+              int num1 = 0,num2 = 0;
+              double res1 = 0,res2=0, num3 = 0, num4 = 0;
+              num1 = Convert.ToInt32(jArray[0][2]);
+              num2 = Convert.ToInt32(jArray[0][3]);
+              res1 = num1 + num2;
+              for (a=1;a<jArray.Length;a++)
+              {
+                  for(b=2;b<jArray[a].Length;b++)
+                  {
+                      num3 = Convert.ToDouble(jArray[a][2]);
+                      num4 = Convert.ToDouble(jArray[a][3]);
+                      res2 = num3 + num4;
+                      if (res1>res2)
+                      {
+                          temp = jArray[a - 1][0];
+                          continue;
+                      }
+                      else
+                      {
+                          temp = jArray[a][0];
+                      }
+                  }
+              }
+
+
+           int r = result.Max();
+           txtResult.Text = r.ToString();
+
+              */
